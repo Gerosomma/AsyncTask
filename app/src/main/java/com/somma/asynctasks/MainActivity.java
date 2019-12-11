403ae9ec31e3e19ada356a866e5d2b70a33c4153
Geronimo.Somma <sommagero@gsoft.com.uy>
2019-12-11 18:56:44 -0300

    commit inicial async tasks

---
+++ app/src/main/java/com/somma/asynctasks/MainActivity.java
@@ -0,0 +1,21 @@
+package com.somma.asynctasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnProcesarOnClick(View view) {
+    }
+
+    public void btnRestaurarOnClick(View view) {
+    }
+}
